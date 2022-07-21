8e1155516fe84b309269cce7a35a8797f278093e
GolovaDanyla <bussinestarget4you@gmail.com>
2022-07-21 18:21:53 +0300

    commit 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chest';
+  cells: any[] = [];
+  figures: string[] = ['♔', '♕', '♖', '♗', '♘', '♙', '♚', '♛', '♜', '♝', '♞', '♟'];
+  constructor() {
+    this.cells = Array.from({ length: 64 }, () => '');
+  }
+
+  pos(i: number) {
+    const row = Math.floor(i / 8) % 2;
+    return i % 2 == +(row === 0);
+  }
 }
